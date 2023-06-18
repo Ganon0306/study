228814c1dad90ab46695028fa6fdffae9615da7a
Ganon0306 <134994629+Ganon0306@users.noreply.github.com>
2023-06-19 01:55:59 +0900

    [level 0] Title: 배열 자르기, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+      int length = num2 - num1 + 1;
+    int[] result = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = numbers[num1 + i];
+    }
+
+    return result;
+    }
+}
