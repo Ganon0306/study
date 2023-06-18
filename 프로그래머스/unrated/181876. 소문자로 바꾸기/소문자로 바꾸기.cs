059331e6fc611b07c4f0efd47fb10432cd78407a
Ganon0306 <134994629+Ganon0306@users.noreply.github.com>
2023-06-19 01:48:36 +0900

    [unrated] Title: 소문자로 바꾸기, Time: 0.73 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.cs
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.cs
@@ -2,7 +2,10 @@ using System;
 
 public class Solution {
     public string solution(string myString) {
-        string answer = "";
+       string answer = "";
+        
+        answer = myString.ToLower();
+        
         return answer;
     }
 }
