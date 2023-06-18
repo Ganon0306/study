ab96fdf6819aaa00443cce59ca37ac1c4c86a56a
Ganon0306 <134994629+Ganon0306@users.noreply.github.com>
2023-06-19 01:50:20 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int[] array, int height) {
+int answer = 0;
+    foreach (int a in array)
+    {
+        if (a > height)
+            answer++;
+    }
+    return answer;
+    }
+}
