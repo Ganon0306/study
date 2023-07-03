925c1ae70347950832c501703af1b8c6b98b2cb8
Ganon0306 <134994629+Ganon0306@users.noreply.github.com>
2023-07-04 01:55:40 +0900

    [unrated] Title: 정수 찾기, Time: 0.44 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list, int n) {
+      {
+        if (Array.IndexOf(num_list, n) != -1)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    }
+}
